46052f07e26e4ad491682cef02d0ea84b05a0ec1
tarik <lonetariq514@gmail.com>
2023-06-25 14:38:52 +0530

    2nd commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,28 @@
+import './ExpenseItem.css'
+
 function ExpenseItem(){
-    return (
+    const expenseDate = new Date (2023, 2, 28);
+    const locationOfExpenditure = 'Bengalore '
+
+    return(
         <div>
-            <div>
-                <div>Food</div>
-                <div>Rs 10</div>
+            <h2 className='expense-item h2'>Expense Item!</h2>
+            <div className='expense-item'>
+                <h2 className='expense-item__description'>Food</h2>
+                <div className='expense-item__description'>{locationOfExpenditure}on {expenseDate.toISOString()}</div>
+                <div className='expense-item__price'>Rs 10</div>
             </div>
-            <div>
-                <div>Petrol</div>
-                <div>Rs 100</div>
+            <div className='expense-item'>
+                <h2 className='expense-item__description'>Petrol</h2>
+                <div className='expense-item__description'>{locationOfExpenditure}on {expenseDate.toISOString()}</div>
+                <div className='expense-item__price'>Rs 100</div>
             </div>
-            <div>
-                <div>Movies</div>
-                <div>Rs 200</div>
+            <div className='expense-item'>
+                <h2 className='expense-item__description'>Movies</h2>
+                <div className='expense-item__description'>{locationOfExpenditure}on {expenseDate.toISOString()}</div>
+                <div className='expense-item__price'>Rs 200</div>
             </div>
-        </div>  
+        </div> 
     );
 }
 
