0dc74507adf94d537407755636128bb05239f220
tarik <lonetariq514@gmail.com>
2023-06-25 15:17:44 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: "e1", 
+      title: "Food", 
+      location: "Bengalore",
+      date: new Date(2022, 5, 12),
+      amount: 10,
+    },
+    {
+      id: "e2",
+      title: "Petrol",
+      location: "Electronic-city",
+      date: new Date(2022, 6, 12),
+      amount: 200,
+    },
+    { id: "e3", 
+      title: "Movies", 
+      location: "Kings-Cenima",
+      date: new Date(2022, 7, 12),
+      amount: 350,
+    },
+    {
+      id: "e4",
+      title: "Electricity",
+      location: "JC-Nagar",
+      date: new Date(2022, 8, 12),
+      amount: 17350,
+    },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <h2>Expense Items</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        location={expenses[0].location}
+        date={expenses[0].date}
+        amount={expenses[0].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        location={expenses[1].location}
+        date={expenses[1].date}
+        amount={expenses[1].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        location={expenses[2].location}
+        date={expenses[2].date}
+        amount={expenses[2].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        location={expenses[3].location}
+        date={expenses[3].date}
+        amount={expenses[3].amount}
+      ></ExpenseItem>
     </div>
-    
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,28 +1,12 @@
 import './ExpenseItem.css'
 
-function ExpenseItem(){
-    const expenseDate = new Date (2023, 2, 28);
-    const locationOfExpenditure = 'Bengalore '
-
+function ExpenseItem(props){
     return(
-        <div>
-            <h2 className='expense-item h2'>Expense Item!</h2>
-            <div className='expense-item'>
-                <h2 className='expense-item__description'>Food</h2>
-                <div className='expense-item__description'>{locationOfExpenditure}on {expenseDate.toISOString()}</div>
-                <div className='expense-item__price'>Rs 10</div>
-            </div>
-            <div className='expense-item'>
-                <h2 className='expense-item__description'>Petrol</h2>
-                <div className='expense-item__description'>{locationOfExpenditure}on {expenseDate.toISOString()}</div>
-                <div className='expense-item__price'>Rs 100</div>
-            </div>
-            <div className='expense-item'>
-                <h2 className='expense-item__description'>Movies</h2>
-                <div className='expense-item__description'>{locationOfExpenditure}on {expenseDate.toISOString()}</div>
-                <div className='expense-item__price'>Rs 200</div>
-            </div>
-        </div> 
+        <div className='expense-item'>
+            <h2 className='expense-item__description'>{props.title}</h2>
+            <div className='expense-item__description'>{props.location} on {props.date.toISOString()}</div>
+            <div className='expense-item__price'>Rs {props.amount}</div>
+        </div>
     );
 }
 
