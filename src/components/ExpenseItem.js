b5e8e58a29ef499d34abb9f36c087106c611e722
tarik <lonetariq514@gmail.com>
2023-06-25 12:32:39 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div>
+                <div>Food</div>
+                <div>Rs 10</div>
+            </div>
+            <div>
+                <div>Petrol</div>
+                <div>Rs 100</div>
+            </div>
+            <div>
+                <div>Movies</div>
+                <div>Rs 200</div>
+            </div>
+        </div>  
+    );
+}
+
+export default ExpenseItem;
