597f50f2f9ad080fa4ddf16a0821e3d0588ae143
tarik <lonetariq514@gmail.com>
2023-06-25 16:52:49 +0530

    4th commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
+import ExpenseDetails from './ExpenseDetails';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpenseItem(props){
     return(
         <div className='expense-item'>
-            <h2 className='expense-item__description'>{props.title}</h2>
-            <div className='expense-item__description'>{props.location} on {props.date.toISOString()}</div>
-            <div className='expense-item__price'>Rs {props.amount}</div>
+            <ExpenseDate date={props.date} />
+            <ExpenseDetails amount={props.amount} location={props.location} title={props.title} />
         </div>
     );
 }
