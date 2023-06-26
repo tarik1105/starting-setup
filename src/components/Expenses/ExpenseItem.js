255d8aa449255b4df215a79e1373625e93a93117
tarik <lonetariq514@gmail.com>
2023-06-26 23:09:34 +0530

    7th commit added delete

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,10 +5,15 @@ import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 const ExpenseItem=(props) => {
+    const deleteHandler = () => {
+        console.log('Deleted');
+    };
+
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <ExpenseDetails amount={props.amount} location={props.location} title={props.title} />
+            <button onClick={deleteHandler}>Delete</button>
         </Card>
     );
 }
