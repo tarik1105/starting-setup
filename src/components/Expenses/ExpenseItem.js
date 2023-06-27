b4edbd281fb0977c2e923c22f14b0c1afebcf7f3
tarik <lonetariq514@gmail.com>
2023-06-27 22:15:29 +0530

    8th commit, added button to update price and title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
 import ExpenseDetails from './ExpenseDetails';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 const ExpenseItem=(props) => {
-    const deleteHandler = () => {
-        console.log('Deleted');
+    const [title, setTitle] = useState(props.title);
+    const [amount, setAmount] = useState(props.amount);
+
+    const editHandler = () => {
+        setTitle('Updated!');
+        console.log(title);
     };
+
+    const updateHandler = () => {
+        setAmount('100');
+        console.log(amount);
+    }
 
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
-            <ExpenseDetails amount={props.amount} location={props.location} title={props.title} />
-            <button onClick={deleteHandler}>Delete</button>
+            <ExpenseDetails amount={amount} location={props.location} title={title} />
+            <button onClick={editHandler}>Edit</button>
+            <button onClick={updateHandler}>Update</button>
         </Card>
     );
 }
