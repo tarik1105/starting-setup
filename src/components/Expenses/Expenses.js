e128e0c58421a2663a3c49a6a1e981a5b82f8e10
tarik <lonetariq514@gmail.com>
2023-07-01 13:07:37 +0530

    14th commit, added conditional rendering & Chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
 
       {/* <ExpenseItem
